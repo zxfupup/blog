c97f1e55e74c770f7b39e1163c5942f573f50bc3
zhangxinfang <zhangxinfang1991@126.com>
2018-04-10 18:04:52 +0800

    自定义指令
    自定义指令

--- src/components/ShowBlogs.vue
+++ src/components/ShowBlogs.vue
@@ -1,8 +1,8 @@
 <template>
-  <div id="show-blogs">
-      <h1>博客总览</h1>
-      <div class="single-blog" v-for="blog in blogs">
-      	<h2>{{blog.title}}</h2>
+  <div v-theme:column="'narrow'" id="show-blogs">
+      <h1 >博客总览</h1>
+      <div  class="single-blog" v-for="blog in blogs">
+      	<h2 v-rainbow>{{blog.title}}</h2>
       	<article>
       		{{blog.body}}
       	</article>
@@ -19,7 +19,7 @@ export default {
   	}
   },
   created(){
-  	this.$http.get("http://jsonplaceholder.typicode.com/posts").then(function(data){
+  	this.$http.get("./../static/posts.json").then(function(data){
   			// console.log(data);
   			this.blogs=data.body.slice(0,10);
   			console.log(this.blogs)
--- src/main.js
+++ src/main.js
@@ -6,7 +6,25 @@ import App from './App'
 
 Vue.config.productionTip = false
 Vue.use(VueResource)
-
+//自定义指令
+Vue.directive('rainbow',{
+	bind:function(el,binding,vnode){
+		el.style.color="#"+Math.random().toString(16).slice(2,8);
+	}
+})
+Vue.directive('theme',{
+	bind:function(el,binding,vnode){
+		if(binding.value=='wide'){
+			el.style.maxWidth="1260px";
+		}else if(binding.value=="narrow"){
+			el.style.maxWidth="560px";
+		}
+		if(binding.arg=="column"){
+			el.style.background="#6677cc";
+			el.style.padding="20px";
+		}
+	}
+})
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
