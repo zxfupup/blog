e14976de81a9becd4eb87a30691f1dcecc6a2017
zhangxinfang <zhangxinfang1991@126.com>
2018-04-11 18:01:15 +0800

    简易完整页面但无数据库
    简易完整页面但无数据库

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,25 @@
 <template>
   <div id="app">
      <!-- <add-blog></add-blog> -->
-     <show-blogs></show-blogs>
+     <!-- <show-blogs></show-blogs> -->
+     <blog-header></blog-header>
+     <router-view></router-view>
   </div>
 </template>
 
 <script>
 import AddBlog from './components/AddBlog'
 import ShowBlogs from './components/ShowBlogs'
+import BlogHeader from './components/BlogHeader'
+import SingleBlog from "./components/SingleBlog"
 export default {
   name: 'App',
   components: {
-    AddBlog,ShowBlogs
+    AddBlog,ShowBlogs,BlogHeader,SingleBlog
   }
 }
 </script>
 
 <style>
- 
+
 </style>
--- src/components/ShowBlogs.vue
+++ src/components/ShowBlogs.vue
@@ -1,10 +1,15 @@
 <template>
   <div v-theme:column="'narrow'" id="show-blogs">
       <h1 >博客总览</h1>
-      <div  class="single-blog" v-for="blog in blogs">
-      	<h2 v-rainbow>{{blog.title}}</h2>
+      <input type="text" v-model="search" placeholder="搜索">
+      <div  class="single-blog" v-for="blog in filteredBlogs">
+      	<router-link v-bind:to="'/blog/'+blog.id">
+          <h2 v-rainbow>
+          {{blog.title | to-uppercase}}
+        </h2>
+        </router-link>
       	<article>
-      		{{blog.body}}
+      		{{blog.body | snippet}}
       	</article>
       </div>
   </div>
@@ -15,15 +20,35 @@ export default {
   name: 'show-blogs',
   data(){
   	return {
-  		blogs:[]
+  		blogs:[],
+      search:""
   	}
   },
   created(){
   	this.$http.get("./../static/posts.json").then(function(data){
   			// console.log(data);
   			this.blogs=data.body.slice(0,10);
-  			console.log(this.blogs)
+  			// console.log(this.blogs)
   	})
+  },
+  computed:{
+    filteredBlogs:function(){
+      return this.blogs.filter((blog)=>{
+        return blog.title.match(this.search)
+      })
+    }
+  },
+  filters:{
+    "to-uppercase":function(value){
+      return value.toUpperCase();//局部过滤器
+    }
+  },
+  directives:{
+    'rainbow':{
+      bind:function(el,binding,vnode){
+      el.style.color="#"+Math.random().toString(16).slice(2,8);
+      }
+    }
   }
 }
 </script>
@@ -38,5 +63,14 @@ export default {
  	margin: 20px 0;
  	box-sizing: border-box;
  	background: #eee;
+
+ }
+ #show-blogs a{
+  color: #444;
+  text-decoration: none;
+ }
+ input[type="text"]{
+  padding: 8px;
+  width: 20%;
  }
 </style>
